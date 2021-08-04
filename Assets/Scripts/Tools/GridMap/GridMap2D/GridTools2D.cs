6053a58d4dffc00f807ff3c5775f5d127fdb4b13
Harryon <Harryonismyname@users.noreply.github.com>
2021-08-04 11:55:22 -0500

    Added DebugCell() and GetNeighborList()

--- Assets/Scripts/Tools/GridMap/GridMap2D/GridTools2D.cs
+++ Assets/Scripts/Tools/GridMap/GridMap2D/GridTools2D.cs
@@ -40,4 +40,54 @@ public static class GridTools2D<TGridObject>
         GetXY(grid, location, grid.orientation, out int x, out int y);
         return IsValidCell(grid, x, y);
     }
+    public static List<TGridObject> GetNeighborList(GridMap2D<TGridObject> grid, Vector3 nodePosition)
+    {
+        List<TGridObject> neighborList = new List<TGridObject>();
+        GetXY(grid, nodePosition, grid.orientation, out int x, out int y);
+        TGridObject GetNode(int nodeX, int nodeY)
+        {
+            if (IsValidCell(grid, nodeX, nodeY)) return grid.GetGridObject(nodeX, nodeY);
+            else return default;
+        }
+        if (x - 1 >= 0)
+        {
+            // Left
+            if (IsValidCell(grid, x - 1, y)) neighborList.Add(GetNode(x - 1, y));
+            // Left Down
+            if (y - 1 >= 0 && IsValidCell(grid, x - 1, y - 1)) neighborList.Add(GetNode(x - 1, y - 1));
+            // Left Up
+            if (y + 1 < grid.Height && IsValidCell(grid, x - 1, y + 1)) neighborList.Add(GetNode(x - 1, y + 1));
+        }
+        if (x + 1 < grid.Width)
+        {
+            // Right
+            if (IsValidCell(grid, x + 1, y)) neighborList.Add(GetNode(x + 1, y));
+            // Right Down
+            if (y - 1 >= 0 && IsValidCell(grid, x + 1, y - 1)) neighborList.Add(GetNode(x + 1, y - 1));
+            // Right Up
+            if (y + 1 < grid.Height && IsValidCell(grid, x + 1, y + 1)) neighborList.Add(GetNode(x + 1, y + 1));
+        }
+        // Down
+        if (y - 1 >= 0 && IsValidCell(grid, x, y - 1)) neighborList.Add(GetNode(x, y - 1));
+        // Up
+        if (y + 1 < grid.Height && IsValidCell(grid, x, y + 1)) neighborList.Add(GetNode(x, y + 1));
+        return neighborList;
+    }
+    public static void DebugCell(GridMap2D<TGridObject> map, Vector3 location, Color color)
+    {
+        GetXY(map, location, map.orientation, out int x, out int y);
+        Vector3 leftCorner = map.GetWorldPosition(x, y);
+        Vector3 rightCorner = map.GetWorldPosition(x, y);
+        rightCorner.x += map.cellSize;
+        rightCorner.y += map.cellSize;
+        Debug.DrawLine(leftCorner, rightCorner, color, 100f);
+    }
+    public static void DebugCell(GridMap2D<TGridObject> map, int x, int y, Color color)
+    {
+        Vector3 leftCorner = map.GetWorldPosition(x, y);
+        Vector3 rightCorner = map.GetWorldPosition(x, y);
+        rightCorner.x += map.cellSize;
+        rightCorner.y += map.cellSize;
+        Debug.DrawLine(leftCorner, rightCorner, color, 100f);
+    }
 }
