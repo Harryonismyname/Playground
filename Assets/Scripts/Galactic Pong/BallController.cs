84b8d4e45e3a6355c2ae3c30f486665ba341c498
Harryon <Harryonismyname@users.noreply.github.com>
2021-08-04 14:39:54 -0500

    Documented and added random trajectory to ball

--- Assets/Scripts/Galactic Pong/BallController.cs
+++ Assets/Scripts/Galactic Pong/BallController.cs
@@ -15,25 +15,25 @@ public class BallController : MonoBehaviour
         transform.position = new Vector3(0, 0, 0);
         StartBall();
     }
-
+    /// <summary>Initializes the ball in the scene with a starting force</summary>
     private void StartBall()
     {
         float value = Random.value;
-        if(value > 0.5f)
+        if (value > 0.5f)
         {
-            body.AddForce(new Vector2(25, -15)*speed);
+            body.AddForce(new Vector2(25, Random.Range(-15, 15)) * speed);
         }
         if (value < 0.5f)
         {
-            body.AddForce(new Vector2(-25, -15)*speed);
+            body.AddForce(new Vector2(-25, Random.Range(-15, 15)) * speed);
         }
     }
-
+    /// <summary>Upon collision change velocity of the ball by a random value</summary>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         audioSource.Play();
         Vector2 vel;
-        if(body.velocity.x > 0f)
+        if (body.velocity.x > 0f)
         {
             vel.x = body.velocity.x + Random.Range(0f, 10f);
         }
@@ -45,7 +45,7 @@ public class BallController : MonoBehaviour
         {
             vel.x = body.velocity.x + Random.Range(-0.1f, 0.1f);
         }
-        if(body.velocity.y > 0f)
+        if (body.velocity.y > 0f)
         {
             vel.y = body.velocity.y + Random.Range(0f, 10f) + (collision.collider.attachedRigidbody.velocity.y / 3);
         }
