51c33293090a5d77e684b206581c02aec5d76dd3
Harryon <Harryonismyname@users.noreply.github.com>
2021-08-04 12:47:03 -0500

    Added documentation to Axis.cs

--- Assets/Scripts/Tools/GridMap/GridMap2D/Axis.cs
+++ Assets/Scripts/Tools/GridMap/GridMap2D/Axis.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// enum responsible for traking 2D grid orientation
+/// </summary>
 public enum Axis
 {
     X,
