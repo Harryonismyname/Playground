5a5918cb1b3331cd718981410ab955aa4305bc0d
Harryon <Harryonismyname@users.noreply.github.com>
2021-08-09 14:55:47 -0500

    Documented TileType.cs

--- Assets/Scripts/EconomySim/TileType.cs
+++ Assets/Scripts/EconomySim/TileType.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>Custom enum to track types of tiles in EconomySim</summary>
 public enum TileType
 {
     None,
