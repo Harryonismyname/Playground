ff0abc048e87f780026fb391d468c3848f1b0407
Harryon <Harryonismyname@users.noreply.github.com>
2021-08-10 12:43:04 -0500

    Created SpriteUV.cs enum

---
+++ Assets/Scripts/Tools/Visualizers/MeshGenerator/SpriteUV.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SpriteUV<TEnum>
+{
+    public TEnum state;
+    public Vector2Int uv00Pixels;
+    public Vector2Int uv11Pixels;
+}
